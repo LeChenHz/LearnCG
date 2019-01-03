eeea8d7d95bd0e955854ab3e431337fcdc3ace75
WanLittle <2621996061@qq.com>
2019-01-03 18:05:09 +0800

    对PET建模完成，晶格数据准确性有待验证（画出来看看）

--- LearnCG/src/scenes/project/pet/PetMathmaticalModel.cpp
+++ LearnCG/src/scenes/project/pet/PetMathmaticalModel.cpp
@@ -3,6 +3,10 @@
 PetMathmaticalModel::PetMathmaticalModel()
 {
 	BDM_number = 12;
+	radius = 10.48f / 2.0f;
+	DU_number_perBDM = 4;
+	Crystall_number_perDU = 13;
+
 	glm::vec3 v_pos;
 	float x, y = 0.0f, z;
 	float angle_segment = TWO_PI / BDM_number;
@@ -15,14 +19,15 @@ PetMathmaticalModel::PetMathmaticalModel()
 		vertexs_pos.push_back(v_pos);
 	}
 	BDM_width = glm::distance(vertexs_pos[0], vertexs_pos[1]);
-	BDM_length = 4 * BDM_width;
+	BDM_length = DU_number_perBDM * BDM_width;
 
+	//对每个探测器进行建模
 	glm::vec3 y_offset = up_normal_vec * BDM_length / 2.0f;
 	for (int i = 0; i < BDM_number; i++)
 	{
 		BDM_model bdm;
 		bdm.center = (vertexs_pos[i] + vertexs_pos[i + 1]) / 2.0f;
-		bdm.right_normal_vec = glm::normalize(vertexs_pos[i + 1] - vertexs_pos[i]);
+		bdm.right_normal_vec = glm::normalize(vertexs_pos[i] - vertexs_pos[i + 1]);
 		glm::vec3 xz_offset = bdm.right_normal_vec * BDM_width / 2.0f;
 		bdm.v1 = bdm.center - xz_offset + y_offset;
 		bdm.v2 = bdm.center + xz_offset + y_offset;
@@ -31,18 +36,61 @@ PetMathmaticalModel::PetMathmaticalModel()
 		BDMs.push_back(bdm);
 	}
 
+	// 对探测器里的探头进行建模
+	for (int i = 0; i < BDM_number; i++)
+	{
+		DU_model du;
+		du.v1 = BDMs[i].v1;
+		du.v2 = BDMs[i].v2;
+		glm::vec3 bdm_v1tov4 = (BDMs[i].v4 - BDMs[i].v1) / glm::vec3(DU_number_perBDM);
+		glm::vec3 bdm_v2tov3 = (BDMs[i].v3 - BDMs[i].v2) / glm::vec3(DU_number_perBDM);
+		du.v3 = du.v2 + bdm_v2tov3;
+		du.v4 = du.v1 + bdm_v1tov4;
+		BDMs[i].DUs.push_back(du);
+		for (int j = 1; j < DU_number_perBDM; j++)
+		{
+			du.v1 += bdm_v1tov4;
+			du.v2 += bdm_v2tov3;
+			du.v3 += bdm_v2tov3;
+			du.v4 += bdm_v1tov4;
+			BDMs[i].DUs.push_back(du);
+		}
+	}
+
+	// 对探头里的晶格进行建模
+	Crystal_model crystal;
+	for (int i = 0; i < BDM_number; i++)
+	{
+		for (int j = 0; j < DU_number_perBDM; j++)
+		{
+			glm::vec3 du_v1 = BDMs[i].DUs[j].v1;
+			glm::vec3 du_v1tov2 = (BDMs[i].v2 - BDMs[i].v1) / glm::vec3(Crystall_number_perDU);
+			glm::vec3 du_v1tov4 = (BDMs[i].v4 - BDMs[i].v1) / glm::vec3(Crystall_number_perDU);
+			du_v1 += du_v1tov2 / glm::vec3(2.0f); //晶格1
+			for (int k = 0; k < Crystall_number_perDU * Crystall_number_perDU; k++)
+			{
+
+				crystal.center = du_v1 + du_v1tov2 * glm::vec3(k % Crystall_number_perDU) + 
+					du_v1tov4 * glm::vec3(k / Crystall_number_perDU);
+				BDMs[i].DUs[j].CRYSTALs.push_back(crystal);
+			}
+		}
+	}
+
 	init();
 }
 
 PetMathmaticalModel::~PetMathmaticalModel()
 {
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	delete g_bdm_position_data;
+	glDeleteVertexArrays(1, &BDM_VAO);
+	glDeleteBuffers(1, &BDM_VBO);
+	delete[] g_bdm_position_data;
+	delete[] g_du_position_data;
 }
 
 void PetMathmaticalModel::init()
 {
+	//将探测器顶点数据填充到数组
 	g_bdm_position_data = new GLfloat[BDM_number * 6 * 3];
 	for (int i = 0; i < BDM_number; i++)
 	{
@@ -61,26 +109,64 @@ void PetMathmaticalModel::init()
 		}
 	}
 
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	
-	glBindVertexArray(VAO);
-	
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * BDM_number * 6 * 3, g_bdm_position_data, GL_STATIC_DRAW);
+	//将探头顶点数据填充到数组
+	g_du_position_data = new GLfloat[BDM_number * 6 * 3 * DU_number_perBDM];
+	for (int i = 0; i < BDM_number; i++)
+	{
+		for (int j = 0; j < DU_number_perBDM; j++)
+		{
+			for (int k = 0; k < 6; k++)
+			{
+				glm::vec3 v;
+				if (k == 0) v = BDMs[i].DUs[j].v1;
+				else if (k == 1) v = BDMs[i].DUs[j].v2;
+				else if (k == 2) v = BDMs[i].DUs[j].v3;
+				else if (k == 3) v = BDMs[i].DUs[j].v1;
+				else if (k == 4) v = BDMs[i].DUs[j].v3;
+				else if (k == 5) v = BDMs[i].DUs[j].v4;
+				g_du_position_data[i * DU_number_perBDM * 6 * 3 + j * 6 * 3 + k * 3 + 0] = v.x;
+				g_du_position_data[i * DU_number_perBDM * 6 * 3 + j * 6 * 3 + k * 3 + 1] = v.y;
+				g_du_position_data[i * DU_number_perBDM * 6 * 3 + j * 6 * 3 + k * 3 + 2] = v.z;
+			}
+		}
+	}
 
+
+	glGenVertexArrays(1, &BDM_VAO);
+	glGenBuffers(1, &BDM_VBO);
+	glBindVertexArray(BDM_VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, BDM_VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * BDM_number * 6 * 3, g_bdm_position_data, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
+
+	glGenVertexArrays(1, &DU_VAO);
+	glGenBuffers(1, &DU_VBO);
+	glBindVertexArray(DU_VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, DU_VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * BDM_number * DU_number_perBDM * 6 * 3, g_du_position_data, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
 }
 
 void PetMathmaticalModel::draw(Shader *shader)
 {
 	shader->use();
-	glBindVertexArray(VAO);
+	shader->setVec3("color", 161.0f/255, 161.0f/255, 123.0f/255);
+	glBindVertexArray(BDM_VAO);
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	glDrawArrays(GL_TRIANGLES, 0, BDM_number * 6 * 3);
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	shader->setVec3("color", 255.0f/255, 255.0f/255, 255.0f/255);
+	glBindVertexArray(DU_VAO);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glDrawArrays(GL_TRIANGLES, 0, BDM_number * DU_number_perBDM * 6 * 3);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
 }
--- LearnCG/src/scenes/project/pet/PetMathmaticalModel.h
+++ LearnCG/src/scenes/project/pet/PetMathmaticalModel.h
@@ -9,11 +9,11 @@ struct Crystal_model {
 };
 
 struct DU_model {
-	glm::vec3 v1, v2, v3, v4, center; //左上 右上 右下 左下
+	glm::vec3 v1, v2, v3, v4; //左上 右上 右下 左下
 	std::vector<Crystal_model> CRYSTALs;
 };
 struct BDM_model {
-	glm::vec3 v1,v2, v3, v4, center; //左上 右上 右下 左下
+	glm::vec3 v1,v2, v3, v4, center; //左上 右上 右下 左下 中心
 	glm::vec3 right_normal_vec; //探测器的右单位向量
 	std::vector<DU_model> DUs;
 };
@@ -25,22 +25,24 @@ public:
 	~PetMathmaticalModel();
 public:
 	int BDM_number; //探测器的个数
-	float radius = 10.48f / 2.0f; //探测器直径
+	float radius; //探测器半径
 	float BDM_width; //探测器宽，对应多边形边长
-	float BDM_length;
+	float BDM_length; // DU_number_perBDM * BDM_width
 
-	int DU_number = 4; // 探头个数
-	int Crystall = 13; //晶体个数 13*13
+	int DU_number_perBDM; // 每个探测器探头个数
+	int Crystall_number_perDU; // 每个探头晶体密度
 
 	glm::vec3 center_pos = glm::vec3(0.0f, 0.0f, 0.0f); //设备中心点
-	glm::vec3 up_normal_vec = glm::vec3(0.0, 1.0f, 0.0f);
+	glm::vec3 up_normal_vec = glm::vec3(0.0, 1.0f, 0.0f); //up单位向量
 
 	std::vector<BDM_model> BDMs;
 	std::vector<glm::vec3> vertexs_pos;
 
 private:
-	GLuint VAO, VBO;
+	GLuint BDM_VAO, BDM_VBO;
+	GLuint DU_VAO, DU_VBO;
 	GLfloat *g_bdm_position_data;
+	GLfloat *g_du_position_data;
 private:
 	void init();
 public:
