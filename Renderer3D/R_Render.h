c0a09eac70698048f2801636e03c7558cd61727a
WanLittle <2621996061@qq.com>
2019-01-05 20:59:54 +0800

    【Rend】了解了程序结构和框架！后续把细节弄清楚

---
+++ Renderer3D/R_Render.h
@@ -0,0 +1,126 @@
+#pragma once
+
+//=====================================================================
+// 渲染实现
+//=====================================================================
+
+#include "R_MATH.h"
+#include "R_CoordinateTransform.h"
+
+void device_draw_primitive(device_t *device, const vertex_t *v1, const vertex_t *v2, const vertex_t *v3);
+void device_render_trap(device_t *device, trapezoid_t *trap);
+void device_draw_scanline(device_t *device, scanline_t *scanline);
+
+// 根据 render_state 绘制原始三角形
+void device_draw_primitive(device_t *device, const vertex_t *v1, const vertex_t *v2, const vertex_t *v3) {
+	point_t p1, p2, p3, c1, c2, c3;
+	int render_state = device->render_state;
+
+	// 按照 Transform 变化
+	transform_apply(&device->transform, &c1, &v1->pos);
+	transform_apply(&device->transform, &c2, &v2->pos);
+	transform_apply(&device->transform, &c3, &v3->pos);
+
+	//TODO
+	// 裁剪，注意此处可以完善为具体判断几个点在 cvv内以及同cvv相交平面的坐标比例
+	// 进行进一步精细裁剪，将一个分解为几个完全处在 cvv内的三角形
+	if (transform_check_cvv(&c1) != 0) 
+		return;
+	if (transform_check_cvv(&c2) != 0) 
+		return;
+	if (transform_check_cvv(&c3) != 0) 
+		return;
+
+	// 归一化
+	transform_homogenize(&device->transform, &p1, &c1);
+	transform_homogenize(&device->transform, &p2, &c2);
+	transform_homogenize(&device->transform, &p3, &c3);
+
+	// 纹理或者色彩绘制
+	if (render_state & (RENDER_STATE_TEXTURE | RENDER_STATE_COLOR)) {
+		vertex_t t1 = *v1, t2 = *v2, t3 = *v3;
+		trapezoid_t traps[2];
+		int n;
+
+		t1.pos = p1;
+		t2.pos = p2;
+		t3.pos = p3;
+		t1.pos.w = c1.w;
+		t2.pos.w = c2.w;
+		t3.pos.w = c3.w;
+
+		vertex_rhw_init(&t1);	// 初始化 w
+		vertex_rhw_init(&t2);	// 初始化 w
+		vertex_rhw_init(&t3);	// 初始化 w
+
+		// 拆分三角形为0-2个梯形，并且返回可用梯形数量
+		n = trapezoid_init_triangle(traps, &t1, &t2, &t3);
+
+		if (n >= 1) 
+			device_render_trap(device, &traps[0]);
+		if (n >= 2) 
+			device_render_trap(device, &traps[1]);
+	}
+
+	if (render_state & RENDER_STATE_WIREFRAME) {		// 线框绘制
+		device_draw_line(device, (int)p1.x, (int)p1.y, (int)p2.x, (int)p2.y, device->foreground);
+		device_draw_line(device, (int)p1.x, (int)p1.y, (int)p3.x, (int)p3.y, device->foreground);
+		device_draw_line(device, (int)p3.x, (int)p3.y, (int)p2.x, (int)p2.y, device->foreground);
+	}
+
+}
+
+// 主渲染函数
+void device_render_trap(device_t *device, trapezoid_t *trap) {
+	scanline_t scanline;
+	int j, top, bottom;
+	top = (int)(trap->top + 0.5f);
+	bottom = (int)(trap->bottom + 0.5f);
+	for (j = top; j < bottom; j++) {
+		if (j >= 0 && j < device->height) {
+			trapezoid_edge_interp(trap, (float)j + 0.5f);
+			trapezoid_init_scan_line(trap, &scanline, j);
+			device_draw_scanline(device, &scanline);
+		}
+		if (j >= device->height) break;
+	}
+}
+
+// 绘制扫描线
+void device_draw_scanline(device_t *device, scanline_t *scanline) {
+	IUINT32 *framebuffer = device->framebuffer[scanline->y];
+	float *zbuffer = device->zbuffer[scanline->y];
+	int x = scanline->x;
+	int w = scanline->w;
+	int width = device->width;
+	int render_state = device->render_state;
+	for (; w > 0; x++, w--) {
+		if (x >= 0 && x < width) {
+			float rhw = scanline->v.rhw;
+			if (rhw >= zbuffer[x]) {
+				float w = 1.0f / rhw;
+				zbuffer[x] = rhw;
+				if (render_state & RENDER_STATE_COLOR) {
+					float r = scanline->v.color.r * w;
+					float g = scanline->v.color.g * w;
+					float b = scanline->v.color.b * w;
+					int R = (int)(r * 255.0f);
+					int G = (int)(g * 255.0f);
+					int B = (int)(b * 255.0f);
+					R = CMID(R, 0, 255);
+					G = CMID(G, 0, 255);
+					B = CMID(B, 0, 255);
+					framebuffer[x] = (R << 16) | (G << 8) | (B);
+				}
+				if (render_state & RENDER_STATE_TEXTURE) {
+					float u = scanline->v.tc.u * w;
+					float v = scanline->v.tc.v * w;
+					IUINT32 cc = device_texture_read(device, u, v);
+					framebuffer[x] = cc;
+				}
+			}
+		}
+		vertex_add(&scanline->v, &scanline->step);
+		if (x >= width) break;
+	}
+}
---
+++ Renderer3D/R_WND.h
@@ -0,0 +1,181 @@
+#pragma once
+
+//=====================================================================
+// Win32 窗口及图形绘制：为 device 提供一个 DibSection 的 FB
+//=====================================================================
+
+#include <windows.h>
+#include <tchar.h>
+
+
+int screen_w, //屏幕宽
+	screen_h, //屏幕高
+	screen_exit = 0;
+int screen_keys[512];	// 当前键盘按下状态
+
+static HWND screen_handle = NULL;		// 主窗口 HWND
+static HDC screen_dc = NULL;			// 配套的 HDC
+static HBITMAP screen_hb = NULL;		// DIB
+static HBITMAP screen_ob = NULL;		// 老的 BITMAP
+
+unsigned char *screen_fb = NULL;		// frame buffer
+long screen_pitch = 0;
+
+int screen_init(int w, int h, const TCHAR *title);	// 屏幕初始化
+int screen_close(void);								// 关闭屏幕
+void screen_dispatch(void);							// 处理消息
+void screen_update(void);							// 显示 FrameBuffer
+
+													// win32 event handler
+static LRESULT screen_events(HWND, UINT, WPARAM, LPARAM);
+
+// 初始化窗口并设置标题
+int screen_init(int w, int h, const TCHAR *title) {
+	//https://baike.baidu.com/item/WNDCLASS/6488732?fr=aladdin
+	//储存某一类窗口的信息
+	WNDCLASS wc = {
+		CS_BYTEALIGNCLIENT, // 窗口类型
+		(WNDPROC)screen_events, //窗口处理函数 lpfnWndProc
+		0, //窗口扩展 cbClsExtra
+		0, //窗口实例扩展 cbWndExtra
+		0, //实例句柄 hInstance
+		NULL,//窗口的最小化图标 hIcon
+		NULL, //窗口鼠标光标 hCursor
+		NULL, //窗口背景色 hbrBackground
+		NULL, //窗口菜单 lpszMenuName
+		_T("SCREEN3.1415926") //窗口类名 lpszClassName 
+	};
+	// https://docs.microsoft.com/zh-cn/windows/desktop/api/wingdi/ns-wingdi-tagbitmapinfo
+	// 设备无关位图文件
+	BITMAPINFO bi = {
+		{ sizeof(BITMAPINFOHEADER), w, -h, 1, 32, BI_RGB, w * h * 4, 0, 0, 0, 0 }
+		//https://docs.microsoft.com/zh-cn/windows/desktop/api/wingdi/ns-wingdi-bitmapinfoheader
+	};
+	RECT rect = { 0, 0, w, h };
+	int wx, wy, sx, sy;
+	LPVOID ptr;
+	// https://baike.baidu.com/item/HDC/16710
+	/* HDC设备上下文是一种包含有关某个设备（如显示器或打印机）的绘制属性信息的 Windows 数据结构。
+	所有绘制调用都通过设备上下文对象进行，这些对象封装了用于绘制线条、形状和文本的 Windows API */
+	HDC hDC;
+
+	screen_close();
+
+	wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wc.hInstance = GetModuleHandle(NULL);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	if (!RegisterClass(&wc)) return -1;
+
+	//https://baike.baidu.com/item/CreateWindow
+	screen_handle = CreateWindow(
+		_T("SCREEN3.1415926"), //LPCTSTR lpClassName 窗口的类名
+		title, //LPCTSTR lpWindowName 指向一个指定窗口名的空结束的字符串指针
+		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, //DWORD dwStyle 指定创建窗口的风格
+		0, //int x 指定窗口的初始水平位置
+		0, //int y 指定窗口的初始垂直位置
+		0, //int nWidth 以设备单元指明窗口的宽度
+		0, //int nHeight 以设备单元指明窗口的高度
+		NULL, //HWND hWndParent 指向被创建窗口的父窗口或所有者窗口的句柄
+		NULL, //HMENU hMenu 菜单句柄
+		wc.hInstance, //HANDLE hlnstance 与窗口相关联的模块实例的句柄
+		NULL //LPVOID lpParam 指向一个值的指针，该值传递给窗口WM_CREATE消息
+	);
+	if (screen_handle == NULL) return -2;
+
+	screen_exit = 0;
+	hDC = GetDC(screen_handle);
+	screen_dc = CreateCompatibleDC(hDC);
+	ReleaseDC(screen_handle, hDC);
+
+	screen_hb = CreateDIBSection(screen_dc, &bi, DIB_RGB_COLORS, &ptr, 0, 0);
+	if (screen_hb == NULL) return -3;
+
+	screen_ob = (HBITMAP)SelectObject(screen_dc, screen_hb);
+	screen_fb = (unsigned char*)ptr;
+	screen_w = w;
+	screen_h = h;
+	screen_pitch = w * 4;
+
+	AdjustWindowRect(&rect, GetWindowLong(screen_handle, GWL_STYLE), 0);
+	wx = rect.right - rect.left;
+	wy = rect.bottom - rect.top;
+	sx = (GetSystemMetrics(SM_CXSCREEN) - wx) / 2;
+	sy = (GetSystemMetrics(SM_CYSCREEN) - wy) / 2;
+	if (sy < 0) sy = 0;
+	SetWindowPos(screen_handle, NULL, sx, sy, wx, wy, (SWP_NOCOPYBITS | SWP_NOZORDER | SWP_SHOWWINDOW));
+	SetForegroundWindow(screen_handle);
+
+	ShowWindow(screen_handle, SW_NORMAL);
+	screen_dispatch();
+
+	memset(screen_keys, 0, sizeof(int) * 512);
+	memset(screen_fb, 0, w * h * 4);
+
+	return 0;
+}
+
+int screen_close(void) {
+	if (screen_dc) {
+		if (screen_ob) {
+			SelectObject(screen_dc, screen_ob);
+			screen_ob = NULL;
+		}
+		DeleteDC(screen_dc);
+		screen_dc = NULL;
+	}
+	if (screen_hb) {
+		DeleteObject(screen_hb);
+		screen_hb = NULL;
+	}
+	if (screen_handle) {
+		CloseWindow(screen_handle);
+		screen_handle = NULL;
+	}
+	return 0;
+}
+
+static LRESULT screen_events(HWND hWnd, UINT msg,
+	WPARAM wParam, LPARAM lParam) {
+	switch (msg) {
+	case WM_CLOSE: screen_exit = 1; break;
+	case WM_KEYDOWN: screen_keys[wParam & 511] = 1; break;
+	case WM_KEYUP: screen_keys[wParam & 511] = 0; break;
+	default: return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+void screen_dispatch(void) {
+	MSG msg;
+	while (1) {
+
+		// 该函数为一个消息检查线程消息队列，并将该消息（如果存在）放于指定的结构
+		if (!PeekMessage(&msg, //接收消息信息的MSG结构指针
+						NULL, //其消息被检查的窗口句柄
+						0, //指定被检查的消息范围里的第一个消息
+						0, //指定被检查的消息范围里的最后一个消息
+						PM_NOREMOVE //确定消息如何被处理。 PM_NOREMOVE：PeekMessage处理后，消息不从队列里除掉。
+						)
+			) 
+			break;
+
+		// 从调用线程的消息队列里取得一个消息并将其放于指定的结构
+		if (!GetMessage(&msg, //指向MSG结构的指针，该结构从线程的消息队列里接收消息信息
+						NULL, //取得其消息的窗口的句柄
+						0, //指定被检索的最小消息值的整数
+						0 //指定被检索的最大消息值的整数
+						)
+			)
+			break;
+		// 该函数分发一个消息给窗口程序
+		DispatchMessage(&msg);
+	}
+}
+
+
+void screen_update(void) {
+	HDC hDC = GetDC(screen_handle);
+	BitBlt(hDC, 0, 0, screen_w, screen_h, screen_dc, 0, 0, SRCCOPY);
+	ReleaseDC(screen_handle, hDC);
+	screen_dispatch();
+}
